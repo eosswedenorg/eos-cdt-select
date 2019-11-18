fcbddd20fd02fd031bdc9ad9d3fdfb07b44724e8
Henrik Hautakoski <henrik@eossweden.org>
2019-11-18 10:07:54 +0100

    scripts/build_deb.sh: change architecture to "all".

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -13,7 +13,7 @@ if [[ -z $RELEASE ]]; then
   RELEASE="1"
 fi
 
-PACKAGE_FULLNAME="${PACKAGE_NAME}_${PACKAGE_VERSION}-${RELEASE}_amd64"
+PACKAGE_FULLNAME="${PACKAGE_NAME}_${PACKAGE_VERSION}-${RELEASE}_all"
 
 rm -fr ${BASE_DIR}/${PACKAGE_TMPDIR}
 
@@ -23,7 +23,7 @@ echo "Package: ${PACKAGE_NAME}
 Version: ${PACKAGE_VERSION}-${RELEASE}
 Section: devel
 Priority: optional
-Architecture: amd64
+Architecture: all
 Homepage: https://github.com/eosswedenorg/eos-cdt-select
 Maintainer: Henrik Hautakoski <henrik@eossweden.org>
 Description: ${PACKAGE_DESCRIPTION}" &> ${BASE_DIR}/${PACKAGE_TMPDIR}/DEBIAN/control
